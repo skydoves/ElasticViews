42e9f409d91146f3761c957fdba2b8369cefa74a
skydoves <skydoves@naver.com>
2018-11-13 18:19:12 +0900

    update suppourt version

--- elasticviews/src/main/java/com/skydoves/elasticviews/ElasticLayout.kt
+++ elasticviews/src/main/java/com/skydoves/elasticviews/ElasticLayout.kt
@@ -33,9 +33,10 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.RelativeLayout
 
-class ElasticLayout : RelativeLayout {
+class ElasticLayout : FrameLayout {
 
     private lateinit var view: View
     private var listener: View.OnClickListener? = null
