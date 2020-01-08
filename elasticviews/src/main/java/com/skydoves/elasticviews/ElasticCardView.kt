667a6282bbe2c3d25797dd5c104dc5060dc439b6
skydoves <skydoves@naver.com>
2020-01-08 19:23:39 +0900

    Implement ElasticCardView

---
+++ elasticviews/src/main/java/com/skydoves/elasticviews/ElasticCardView.kt
@@ -0,0 +1,134 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 skydoves
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.skydoves.elasticviews
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.View.OnClickListener
+import androidx.cardview.widget.CardView
+
+@Suppress("unused", "MemberVisibilityCanBePrivate")
+class ElasticCardView : CardView {
+
+  var scale = 0.9f
+  var duration = 500
+
+  private var onUserClickListener: OnClickListener? = null
+  private var onFinishListener: ElasticFinishListener? = null
+
+  constructor(context: Context) : super(context) {
+    onCreate()
+  }
+
+  constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+    onCreate()
+    getAttrs(attributeSet)
+  }
+
+  constructor(context: Context, attributeSet: AttributeSet, defStyle: Int) : super(context,
+    attributeSet, defStyle) {
+    onCreate()
+    getAttrs(attributeSet, defStyle)
+  }
+
+  private fun onCreate() {
+    super.setOnClickListener {
+      elasticAnimation(this) {
+        setDuration(this@ElasticCardView.duration)
+        setScaleX(this@ElasticCardView.scale)
+        setScaleY(this@ElasticCardView.scale)
+        setOnFinishListener { invokeListeners() }
+      }.doAction()
+    }
+  }
+
+  private fun getAttrs(attrs: AttributeSet) {
+    val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ElasticCardView)
+    try {
+      setTypeArray(typedArray)
+    } finally {
+      typedArray.recycle()
+    }
+  }
+
+  private fun getAttrs(attrs: AttributeSet, defStyle: Int) {
+    val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ElasticCardView, defStyle, 0)
+    try {
+      setTypeArray(typedArray)
+    } finally {
+      typedArray.recycle()
+    }
+  }
+
+  private fun setTypeArray(typedArray: TypedArray) {
+    this.scale = typedArray.getFloat(R.styleable.ElasticCardView_cardView_scale, this.scale)
+    this.duration = typedArray.getInt(R.styleable.ElasticCardView_cardView_duration, this.duration)
+  }
+
+  override fun setOnClickListener(listener: OnClickListener?) {
+    this.onUserClickListener = listener
+  }
+
+  fun setOnFinishListener(listener: ElasticFinishListener) {
+    this.onFinishListener = listener
+  }
+
+  private fun invokeListeners() {
+    this.onUserClickListener?.onClick(this)
+    this.onFinishListener?.onFinished()
+  }
+
+  /** Builder class for creating [ElasticCardView]. */
+  class Builder(context: Context) {
+    private val elasticCardView = ElasticCardView(context)
+
+    fun setScale(value: Float) = apply { this.elasticCardView.scale = value }
+    fun setDuration(value: Int) = apply { this.elasticCardView.duration = value }
+
+    fun setOnClickListener(block: () -> Unit) = apply {
+      val onClickListener = OnClickListener { block() }
+      this.elasticCardView.setOnClickListener(onClickListener)
+    }
+
+    fun setOnClickListener(value: OnClickListener) = apply {
+      this.elasticCardView.setOnClickListener(value)
+    }
+
+    fun setOnFinishListener(block: () -> Unit) = apply {
+      val onElasticFinishListener = object : ElasticFinishListener {
+        override fun onFinished() {
+          block()
+        }
+      }
+      this.elasticCardView.setOnFinishListener(onElasticFinishListener)
+    }
+
+    fun setOnFinishListener(value: ElasticFinishListener) = apply {
+      this.elasticCardView.setOnFinishListener(value)
+    }
+
+    fun build() = this.elasticCardView
+  }
+}
